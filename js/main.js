9eaae7dadfb06ffd7d27279c87a969d5790c9033
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2014-08-09 16:57:54 -0400

    Set up backbone and requirejs

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Require.js allows us to configure shortcut alias
+require.config({
+  // The shim config allows us to configure dependencies for
+  // scripts that do not call define() to register a module
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: [
+        'underscore',
+        'jquery'
+      ],
+      exports: 'Backbone'
+    }
+  },
+  paths: {
+    jquery: '../bower_components/jquery/jquery',
+    underscore: '../bower_components/underscore/underscore',
+    backbone: '../bower_components/backbone/backbone'
+  }
+});
