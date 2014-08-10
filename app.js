079149b06abcac515c6a4055e28f9239a5770a05
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2014-08-10 15:31:37 -0400

    Update server to include API routes and body-parser

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
 
+// Configure.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+// Home Route.
 app.get('/', function (req, res) {
   res.render('index.html');
 });
+
+// Use Express Router for api routes.
+app.use('/api', router);
+require('./server/api')(router);
 
 app.listen(3000);
 console.log('Listening on port 3000');
