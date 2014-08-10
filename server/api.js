81d77706fec9be2c8172737fa9ee523900d0c2d8
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2014-08-10 15:30:01 -0400

    Small REST API

---
+++ server/api.js
@@ -0,0 +1,105 @@
+_ = require('underscore');
+
+var Collection, type, err, result,
+    _params, _handleError, _getById, _getAll, _create, _deleteById,
+    urlMap = {
+      bugs:   'bugs',
+      scores: 'scores'
+    },
+    collectionMap = {
+      bugs:   [],
+      scores: []
+    };
+
+_handleError = function(err, res) {
+  return res.send(500, {
+    name:    err.name,
+    message: err.message
+  });
+};
+
+// Read the parameters send with the request.
+_params = function(params, cb) {
+  err   = null;
+  type  = urlMap[params.model];
+  Collection = collectionMap[type];
+
+  if (typeof Collection === 'undefined' || Collection === null) {
+    err = new Error('Collection of type ' + params.Collection + ' not allowed.');
+    return cb(err, null);
+  }
+
+  result = {
+    model: params.model,
+    type:  type,
+    id:    params.id,
+    Collection: Collection
+  };
+  return cb(err, result);
+};
+
+_getById = function(req, res) {
+  _params(req.params, function(err, params) {
+    if (err) {
+      return _handleError(err, res);
+    }
+
+    Collection = params.Collection;
+    res.json(_.where(Collection, {id: params.id}));
+
+  });
+};
+
+_deleteById = function(req, res) {
+  _params(req.params, function(err, params) {
+    if (err) {
+      return _handleError(err, res);
+    }
+
+    Collection = params.Collection;
+    idToDelete = params.id;
+    console.log('ok');
+    // TODO: Filter out the object and return the modified collection.
+    // Collection = Collection.filter(function(obj) {
+    //   return idToDelete.indexOf(obj.id) === -1;
+    // });
+
+    res.json(Collection);
+  });
+};
+
+_getAll = function(req, res) {
+  _params(req.params, function(err, params) {
+    if (err) {
+      return _handleError(err, res);
+    }
+
+    // Send back the entire array of objects.
+    res.json(params.Collection);
+  });
+};
+
+_create = function(req, res) {
+  _params(req.params, function(err, params) {
+    if (err) {
+      return _handleError(err, res);
+    }
+
+    // Add a model to the array.
+    Collection = params.Collection;
+    Collection.push(req.body);
+
+    res.json(Collection);
+  });
+};
+
+// Public API
+module.exports = function(router) {
+  router.route('/:model/:id')
+  .get(_getById)
+  .delete(_deleteById);
+
+  router.route('/:model')
+  .get(_getAll)
+  .post(_create);
+};
