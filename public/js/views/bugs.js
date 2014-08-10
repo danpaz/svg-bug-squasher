4f5235ba3e956178910fb9f9701b41b17f2b94e2
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2014-08-10 16:32:41 -0400

    Earn more points for harder bugs

--- public/js/views/bugs.js
+++ public/js/views/bugs.js
@@ -7,6 +7,24 @@ define([
 ], function ($, _, Backbone, Score, SVG) {
   'use strict';
 
+  var points = function (type) {
+    var numPoints = 0;
+    switch(type) {
+      case 'easy':
+        numPoints = 1;
+        break;
+      case 'medium':
+        numPoints = 3;
+        break;
+      case 'hard':
+        numPoints = 5;
+        break;
+      default:
+        numPoints = 0;
+    }
+    return numPoints;
+  };
+
   var BugView = Backbone.View.extend({
 
     // Use client id to identify bugs.
@@ -29,13 +47,8 @@ define([
 
     squash: function () {
       this.model.squash();
-      var points = 0;
-      console.log(this.model.attributes.difficulty);
-      switch(this.model.difficulty) {
-
-      }
-
-      Score.increment(1);
+      var difficulty = this.model.attributes.difficulty.type;
+      Score.increment(points(difficulty));
     },
 
     // Remove the item, destroy the model, and delete its view.
